a1476f37b188bd108dffa8df201617bab656bc55
spt4m1m <white.hat.tamim@gmail.com>
2023-06-07 12:51:42 +0600

    connected with database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const colors = require('colors');
 const path = require('path');
 require("dotenv").config();
@@ -9,6 +10,36 @@ const app = express();
 // middlewares 
 app.use(cors())
 app.use(express.json())
+
+
+// Database Connection 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zse5se0.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Database Connected".yellow);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
