294f1deb94f88e71c2becf42323540482e5e1e08
spt4m1m <white.hat.tamim@gmail.com>
2023-06-09 00:40:31 +0600

    delete selectedclass api

--- index.js
+++ index.js
@@ -279,6 +279,14 @@ async function run() {
             res.send({ result })
         })
 
+        // delete selected class 
+        app.delete('/selectedclass/delete/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await SelectedClassCollection.deleteOne(query);
+            res.send({ result })
+        })
+
 
         // ------------------ code end about selected class ------------ //
 
